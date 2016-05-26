99c649278e020585430ec32ecbc1088adff958e8
Wang Haoting <hatsuyukiw@gmail.com>
2016-05-26 15:41:35 +0800

    Fixed a bug in model/score.py

--- model/score.py
+++ model/score.py
@@ -5,6 +5,7 @@ import numpy as np
 def score(pred, act):
     (n, m) = pred.shape
     act = act + np.ones(act.shape)
+    pred = pred + np.ones(pred.shape)
     sig = np.sqrt(np.sum(((pred - act) / act) ** 2, axis = 0) / n)
     phi = np.sqrt(np.sum(act, axis = 0))
     f = np.dot(np.ones((m,)) - sig, phi)
