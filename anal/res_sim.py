3107c562ce888b248d51ce21e90daac99ae88ec4
Wang Haoting <hatsuyukiw@gmail.com>
2016-05-27 13:21:37 +0800

    Added a similarity measure and updated the log

--- anal/res_sim.py
+++ anal/res_sim.py
@@ -1,4 +1,15 @@
 __author__ = 'Hatsuyuki'
 
+import numpy as np
+import pandas as pd
+
+def _sim(a, b):
+    x = a[1].values
+    y = b[1].values
+    return np.mean(np.abs(x - y) / (x + y) / 2)
+
+def read_sub(file_name):
+    return pd.read_csv(r'../data/' + file_name, header = None)
+
 def res_sim(a, b):
-    pass
+    return _sim(read_sub(a), read_sub(b))
