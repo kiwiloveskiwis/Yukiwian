Fixed a few bugs and added normalization process in the RF model.

model/random_forest.py
@@ -6,25 +6,40 @@ from sklearn.ensemble import RandomForestRegressor
 
 random_state = 2016
 
+def nor(x, y = 0):
+    x_bar = np.mean(x)
+    s_x = np.sqrt(np.sum((x - np.mean(x)) ** 2))
+    if np.abs(s_x) > 1e-10: return (x - x_bar) / s_x, (y - x_bar) / s_x, x_bar, s_x
+    else: return x - x_bar, y - x_bar, x_bar, 1
+
 ts = pd.read_csv(r'../data/ts.csv', index_col = 0)
 
 pred_range = pd.date_range('20150831', '20151030')
 pred_size = np.size(pred_range)
 pred = pd.DataFrame(data = np.zeros((pred_size, np.size(ts.columns))), index = pred_range, columns = ts.columns)
 
+#songs = pd.read_csv(r'../data/songs.csv')
+
 n_lags = 7
 
+train = np.zeros((ts.shape[0] - n_lags, n_lags))
+y = np.zeros((ts.shape[0] - n_lags,))
+
+c = 0
+
 for songs in ts.columns:
-    clf = RandomForestRegressor(n_estimators = 100, random_state = random_state)
+    print c
+    c += 1
+    clf = RandomForestRegressor(n_estimators = 100, random_state = random_state, n_jobs = 2)
     val = ts[songs].values
-    train = np.array([val[0:n_lags]])
-    y = np.array(val[n_lags])
-    for i in np.arange(1, np.size(val) - n_lags):
-        train = np.concatenate((train, [val[i:i + n_lags]]))
-        y = np.append(y, val[i + n_lags])
+    for i in np.arange(0, np.size(val) - n_lags):
+        train[i] ,y[i], x_bar, s_x = nor(val[i:i + n_lags], val[i + n_lags])
     clf.fit(train, y)
     for i in np.arange(pred_size):
-        val = np.append(val, clf.predict([val[-n_lags:]]))
+        x, y_t, x_bar, s_x = nor(val[-n_lags:])
+        y_t = clf.predict([x])
+        y_t = y_t * s_x + x_bar
+        val = np.append(val, y_t)
     pred[songs] = val[-pred_size:]
 
-pred.to_csv('pred.csv', index = False)
+pred.to_csv(r'../data/pred.csv')

model/random_forest_with_val.py
@@ -35,7 +35,7 @@ for songs in ts.columns:
         val = np.append(val, clf.predict([val[-n_lags:]]))
     valid[songs] = val[-valid_size:]
 
-valid.to_csv(r'../data/valid.csv', index = False)
+valid.to_csv(r'../data/valid.csv')
 
 sco = score(create_submission(pred = valid).values, ts_for_artists.loc[valid_range].values)
 print sco